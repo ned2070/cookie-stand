f89c341cb7422fa9f1cd334cc373ad644f25a6e2
ned2070te <ned2070te@gmail.com>
2023-09-28 12:53:54 +0100

    html working no css

--- app.js
+++ app.js
@@ -43,6 +43,9 @@ Cookieshop.prototype.calcCookiesSold = function () {
     // fill cookiesPerHour
     const cookiesHourly = Math.floor(customHourly * this.cookiesPerCustom);
     this.cookiesPerHour.push(cookiesHourly);
+
+    // calculate cookiesPerDay
+    this.cookiesPerDay = this.cookiesPerDay + cookiesHourly;
   }
 };
 
@@ -59,6 +62,70 @@ tokyo.calcCookiesSold();
 dubai.calcCookiesSold();
 paris.calcCookiesSold();
 lima.calcCookiesSold();
+
+// get the table from the HTML so we can add rows
+const table = document.getElementById("shopData");
+
+Cookieshop.prototype.render = function () {
+  // create a row
+  const tr = document.createElement("tr");
+
+  // add the store name to the row
+  const th = document.createElement("th");
+  th.textContent = this.shopLocation;
+  tr.appendChild(th);
+
+  // add this store's data to that row
+  for (let i = 0; i < this.cookiesPerHour.length; i++) {
+    // create a new td and put the sales number in it
+    const td = document.createElement("td");
+    td.textContent = this.cookiesPerHour[i];
+    tr.appendChild(td);
+  }
+
+  // add the total to the end of the row
+  const totalTd = document.createElement("td");
+  totalTd.textContent = this.cookiesPerDay;
+  tr.appendChild(totalTd);
+
+  // add that row to the table
+  table.appendChild(tr);
+};
+
+// claculate sales for each store (commented out because the calculate sales in the render method)
+// seattle.calculateSales()
+// tokyo.calculateSales()
+// dubai.calculateSales()
+// paris.calculateSales()
+// lima.calculateSales()
+
+// render the header row
+// create the tr
+const headerRow = document.createElement("tr");
+const blankTd = document.createElement("td");
+headerRow.appendChild(blankTd);
+
+// add each time in a th
+for (let i = 0; i < hours.length; i++) {
+  const th = document.createElement("th");
+  th.textContent = hours[i];
+  headerRow.appendChild(th);
+}
+
+// add a total heading
+const totalHeading = document.createElement("th");
+totalHeading.textContent = "Total";
+headerRow.appendChild(totalHeading);
+
+// add the row to the table
+table.appendChild(headerRow);
+
+// render each store on the page
+seattle.render();
+tokyo.render();
+dubai.render();
+paris.render();
+lima.render();
 
 // test arrays
 //console.log(seattle.customPerHour);
